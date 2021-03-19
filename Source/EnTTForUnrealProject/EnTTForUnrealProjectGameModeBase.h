fccb921ebfb15824f0a1e9b57232b3d735f20874
Bent Nürnberg <bent@gravinatorgames.com>
2021-03-19 22:29:36 +0100

    Added Unreal Project

---
+++ Source/EnTTForUnrealProject/EnTTForUnrealProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EnTTForUnrealProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENTTFORUNREALPROJECT_API AEnTTForUnrealProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
